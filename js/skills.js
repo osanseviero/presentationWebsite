f75fbd50be352ebfc6aab1bfa5b6b2d000864588
Omar S <osanseviero@gmail.com>
2016-12-20 11:02:56 -0500

    feat: Change the image depending on the skill

--- js/skills.js
+++ js/skills.js
@@ -1,42 +1,47 @@
 var skills = [
 	{
-		'name' : 'Web Develpment',
+		'name' : 'Web Development',
 		'description' : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate perferendis quos architecto reprehenderit quam pariatur dolor obcaecati veritatis ad soluta impedit reiciendis, eos nulla officia similique, voluptatum esse quas magnam.",
-		'skills' : ['HTML', 'CSS'] 
+		'skills' : ['HTML', 'CSS'],
+		'image' : 'http://placehold.it/300x150/FFC107/000000'
 		
 	},
 	{
 		'name' : 'Video Game Devopment',
 		'description' : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate perferendis quos architecto reprehenderit quam pariatur dolor obcaecati veritatis ad soluta impedit reiciendis, eos nulla officia similique, voluptatum esse quas magnam.",
-		'skills' : ['Unity', 'Vuforia'] 
+		'skills' : ['Unity', 'Vuforia'],
+		'image' : 'http://placehold.it/300x150/55E1FF/000000'
 		
 	},
 	{
 		'name' : 'Video Game Devopment',
 		'description' : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate perferendis quos architecto reprehenderit quam pariatur dolor obcaecati veritatis ad soluta impedit reiciendis, eos nulla officia similique, voluptatum esse quas magnam.",
-		'skills' : ['Unity', 'Vuforia'] 
-		
+		'skills' : ['Unity', 'Vuforia'],
+		'image' : 'http://placehold.it/300x150/63FF51/000000'
 	}
 ];
 
 
-var cardOpening = '<paper-card class="skill" heading="%data%" image="http://placehold.it/300x150/FFC107/000000" alt="%data%"></paper-card>';
+var cardOpening = '<paper-card class="skill" heading="%data%" image="%img%" alt="%data%"></paper-card>';
 var cardContent = '<div class="card-content">%data%</div>';
 
 
 var skillSection = document.getElementById('skills')
 
-var formattedOpening, formattedContent, skill;
+var skillElement, formattedOpening, contentElement, skill, formattedHTML;
 
 for(var i = 0; i < skills.length; i++) {
 	skill = skills[i];
-	formattedOpening = document.createElement('div');
-	formattedOpening.innerHTML = cardOpening.replace('%data%', skill.name);
+	skillElement = document.createElement('div');
 
-	formattedContent = document.createElement('div');
-	formattedContent.innerHTML = cardContent.replace('%data%', skill.description);
+	formattedOpening = cardOpening.replace('%data%', skill.name);
+	formattedOpening = formattedOpening.replace('%img%', skill.image);
+	skillElement.innerHTML = formattedOpening;
 
-	formattedOpening.firstChild.appendChild(formattedContent);
+	contentElement = document.createElement('div');
+	contentElement.innerHTML = cardContent.replace('%data%', skill.description);
 
-	skillSection.appendChild(formattedOpening);
+	skillElement.firstChild.appendChild(contentElement);
+
+	skillSection.appendChild(skillElement);
 }
